ebf54c57b048d68e221346862656734b4ea7d4a9
pknw2 <github@pknw1.co.uk>
2025-06-03 18:06:50 +0100

    1748970410

--- quick-push.sh
+++ quick-push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git add . && git commit -m $(date +%s) && git push
-ssh ks5 'cd /media/www/notflix-jellyfin-embedded/ && sudo git pull'
+ssh ks5 'cd /media/www/notflix-jellyfin-embedded/ && sudo git stash && sudo git pull'
